2ac4bb5adf55f4c460fc70a64442761643e137cf
Brad Rowe <rowebradleyj@gmail.com>
2018-02-27 08:25:17 -0700

    update bedrock

--- site/config/environments/production.php
+++ site/config/environments/production.php
@@ -1,7 +1,6 @@
 <?php
 /** Production */
 ini_set('display_errors', 0);
-define('WP_CACHE', true);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
